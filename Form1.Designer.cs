6ef9684e893305a81b73b03bf002d432ce7e998a
VladutIT <89774521+VladutIT@users.noreply.github.com>
2021-08-30 18:53:34 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,298 @@
+﻿
+namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.ecran = new System.Windows.Forms.Label();
+            this.inmultire = new System.Windows.Forms.Button();
+            this.impartire = new System.Windows.Forms.Button();
+            this.adunare = new System.Windows.Forms.Button();
+            this.scadere = new System.Windows.Forms.Button();
+            this.radical = new System.Windows.Forms.Button();
+            this.egal = new System.Windows.Forms.Button();
+            this.unu = new System.Windows.Forms.Button();
+            this.doi = new System.Windows.Forms.Button();
+            this.trei = new System.Windows.Forms.Button();
+            this.patru = new System.Windows.Forms.Button();
+            this.cinci = new System.Windows.Forms.Button();
+            this.sase = new System.Windows.Forms.Button();
+            this.sapte = new System.Windows.Forms.Button();
+            this.zero = new System.Windows.Forms.Button();
+            this.opt = new System.Windows.Forms.Button();
+            this.noua = new System.Windows.Forms.Button();
+            this.stergere = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Controls.Add(this.ecran);
+            this.panel1.Location = new System.Drawing.Point(26, 33);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(378, 125);
+            this.panel1.TabIndex = 0;
+            // 
+            // ecran
+            // 
+            this.ecran.BackColor = System.Drawing.Color.Black;
+            this.ecran.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ecran.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ecran.Location = new System.Drawing.Point(144, 34);
+            this.ecran.Name = "ecran";
+            this.ecran.Size = new System.Drawing.Size(185, 60);
+            this.ecran.TabIndex = 0;
+            // 
+            // inmultire
+            // 
+            this.inmultire.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("inmultire.BackgroundImage")));
+            this.inmultire.Location = new System.Drawing.Point(12, 186);
+            this.inmultire.Name = "inmultire";
+            this.inmultire.Size = new System.Drawing.Size(100, 98);
+            this.inmultire.TabIndex = 1;
+            this.inmultire.UseVisualStyleBackColor = true;
+            this.inmultire.Click += new System.EventHandler(this.inmultire_Click);
+            // 
+            // impartire
+            // 
+            this.impartire.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("impartire.BackgroundImage")));
+            this.impartire.Location = new System.Drawing.Point(118, 186);
+            this.impartire.Name = "impartire";
+            this.impartire.Size = new System.Drawing.Size(100, 98);
+            this.impartire.TabIndex = 2;
+            this.impartire.UseVisualStyleBackColor = true;
+            this.impartire.Click += new System.EventHandler(this.impartire_Click);
+            // 
+            // adunare
+            // 
+            this.adunare.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("adunare.BackgroundImage")));
+            this.adunare.Location = new System.Drawing.Point(224, 186);
+            this.adunare.Name = "adunare";
+            this.adunare.Size = new System.Drawing.Size(100, 98);
+            this.adunare.TabIndex = 3;
+            this.adunare.UseVisualStyleBackColor = true;
+            this.adunare.Click += new System.EventHandler(this.adunare_Click);
+            // 
+            // scadere
+            // 
+            this.scadere.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("scadere.BackgroundImage")));
+            this.scadere.Location = new System.Drawing.Point(330, 186);
+            this.scadere.Name = "scadere";
+            this.scadere.Size = new System.Drawing.Size(100, 98);
+            this.scadere.TabIndex = 4;
+            this.scadere.UseVisualStyleBackColor = true;
+            this.scadere.Click += new System.EventHandler(this.scadere_Click);
+            // 
+            // radical
+            // 
+            this.radical.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("radical.BackgroundImage")));
+            this.radical.Location = new System.Drawing.Point(12, 290);
+            this.radical.Name = "radical";
+            this.radical.Size = new System.Drawing.Size(100, 98);
+            this.radical.TabIndex = 5;
+            this.radical.UseVisualStyleBackColor = true;
+            this.radical.Click += new System.EventHandler(this.radical_Click);
+            // 
+            // egal
+            // 
+            this.egal.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("egal.BackgroundImage")));
+            this.egal.Location = new System.Drawing.Point(118, 290);
+            this.egal.Name = "egal";
+            this.egal.Size = new System.Drawing.Size(100, 98);
+            this.egal.TabIndex = 6;
+            this.egal.UseVisualStyleBackColor = true;
+            this.egal.Click += new System.EventHandler(this.egal_Click);
+            // 
+            // unu
+            // 
+            this.unu.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("unu.BackgroundImage")));
+            this.unu.Location = new System.Drawing.Point(12, 394);
+            this.unu.Name = "unu";
+            this.unu.Size = new System.Drawing.Size(100, 98);
+            this.unu.TabIndex = 7;
+            this.unu.UseVisualStyleBackColor = true;
+            this.unu.Click += new System.EventHandler(this.unu_Click);
+            // 
+            // doi
+            // 
+            this.doi.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("doi.BackgroundImage")));
+            this.doi.Location = new System.Drawing.Point(118, 394);
+            this.doi.Name = "doi";
+            this.doi.Size = new System.Drawing.Size(100, 98);
+            this.doi.TabIndex = 8;
+            this.doi.UseVisualStyleBackColor = true;
+            this.doi.Click += new System.EventHandler(this.doi_Click);
+            // 
+            // trei
+            // 
+            this.trei.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("trei.BackgroundImage")));
+            this.trei.Location = new System.Drawing.Point(224, 394);
+            this.trei.Name = "trei";
+            this.trei.Size = new System.Drawing.Size(100, 98);
+            this.trei.TabIndex = 9;
+            this.trei.UseVisualStyleBackColor = true;
+            this.trei.Click += new System.EventHandler(this.trei_Click);
+            // 
+            // patru
+            // 
+            this.patru.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("patru.BackgroundImage")));
+            this.patru.Location = new System.Drawing.Point(330, 394);
+            this.patru.Name = "patru";
+            this.patru.Size = new System.Drawing.Size(100, 98);
+            this.patru.TabIndex = 10;
+            this.patru.UseVisualStyleBackColor = true;
+            this.patru.Click += new System.EventHandler(this.patru_Click);
+            // 
+            // cinci
+            // 
+            this.cinci.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("cinci.BackgroundImage")));
+            this.cinci.Location = new System.Drawing.Point(12, 498);
+            this.cinci.Name = "cinci";
+            this.cinci.Size = new System.Drawing.Size(100, 98);
+            this.cinci.TabIndex = 11;
+            this.cinci.UseVisualStyleBackColor = true;
+            this.cinci.Click += new System.EventHandler(this.cinci_Click);
+            // 
+            // sase
+            // 
+            this.sase.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("sase.BackgroundImage")));
+            this.sase.Location = new System.Drawing.Point(118, 498);
+            this.sase.Name = "sase";
+            this.sase.Size = new System.Drawing.Size(100, 98);
+            this.sase.TabIndex = 12;
+            this.sase.UseVisualStyleBackColor = true;
+            this.sase.Click += new System.EventHandler(this.sase_Click);
+            // 
+            // sapte
+            // 
+            this.sapte.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("sapte.BackgroundImage")));
+            this.sapte.Location = new System.Drawing.Point(224, 498);
+            this.sapte.Name = "sapte";
+            this.sapte.Size = new System.Drawing.Size(100, 98);
+            this.sapte.TabIndex = 13;
+            this.sapte.UseVisualStyleBackColor = true;
+            this.sapte.Click += new System.EventHandler(this.sapte_Click);
+            // 
+            // zero
+            // 
+            this.zero.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("zero.BackgroundImage")));
+            this.zero.Location = new System.Drawing.Point(224, 290);
+            this.zero.Name = "zero";
+            this.zero.Size = new System.Drawing.Size(100, 98);
+            this.zero.TabIndex = 14;
+            this.zero.UseVisualStyleBackColor = true;
+            this.zero.Click += new System.EventHandler(this.zero_Click);
+            // 
+            // opt
+            // 
+            this.opt.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("opt.BackgroundImage")));
+            this.opt.Location = new System.Drawing.Point(330, 498);
+            this.opt.Name = "opt";
+            this.opt.Size = new System.Drawing.Size(100, 98);
+            this.opt.TabIndex = 15;
+            this.opt.UseVisualStyleBackColor = true;
+            this.opt.Click += new System.EventHandler(this.opt_Click);
+            // 
+            // noua
+            // 
+            this.noua.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("noua.BackgroundImage")));
+            this.noua.Location = new System.Drawing.Point(170, 602);
+            this.noua.Name = "noua";
+            this.noua.Size = new System.Drawing.Size(100, 98);
+            this.noua.TabIndex = 16;
+            this.noua.UseVisualStyleBackColor = true;
+            this.noua.Click += new System.EventHandler(this.noua_Click);
+            // 
+            // stergere
+            // 
+            this.stergere.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("stergere.BackgroundImage")));
+            this.stergere.Location = new System.Drawing.Point(330, 290);
+            this.stergere.Name = "stergere";
+            this.stergere.Size = new System.Drawing.Size(100, 98);
+            this.stergere.TabIndex = 17;
+            this.stergere.UseVisualStyleBackColor = true;
+            this.stergere.Click += new System.EventHandler(this.stergere_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(442, 707);
+            this.Controls.Add(this.stergere);
+            this.Controls.Add(this.noua);
+            this.Controls.Add(this.opt);
+            this.Controls.Add(this.zero);
+            this.Controls.Add(this.sapte);
+            this.Controls.Add(this.sase);
+            this.Controls.Add(this.cinci);
+            this.Controls.Add(this.patru);
+            this.Controls.Add(this.trei);
+            this.Controls.Add(this.doi);
+            this.Controls.Add(this.unu);
+            this.Controls.Add(this.egal);
+            this.Controls.Add(this.radical);
+            this.Controls.Add(this.scadere);
+            this.Controls.Add(this.adunare);
+            this.Controls.Add(this.impartire);
+            this.Controls.Add(this.inmultire);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label ecran;
+        private System.Windows.Forms.Button inmultire;
+        private System.Windows.Forms.Button impartire;
+        private System.Windows.Forms.Button adunare;
+        private System.Windows.Forms.Button scadere;
+        private System.Windows.Forms.Button radical;
+        private System.Windows.Forms.Button egal;
+        private System.Windows.Forms.Button unu;
+        private System.Windows.Forms.Button doi;
+        private System.Windows.Forms.Button trei;
+        private System.Windows.Forms.Button patru;
+        private System.Windows.Forms.Button cinci;
+        private System.Windows.Forms.Button sase;
+        private System.Windows.Forms.Button sapte;
+        private System.Windows.Forms.Button zero;
+        private System.Windows.Forms.Button opt;
+        private System.Windows.Forms.Button noua;
+        private System.Windows.Forms.Button stergere;
+    }
+}
+
